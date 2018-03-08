25e8963563add986e669b97ee0334b17c237bfb7
Jorge Hevia <jmhev@outlook.com>
2018-03-08 09:10:32 +0100

    Formatted controller.py to pep8 standard

--- controller.py
+++ controller.py
@@ -3,14 +3,15 @@ from nltk.tokenize import word_tokenize
 from pathlib import Path
 from dbmanager import DBManager
 from htmlparser import HTMLParser
-import sys
 
 nltk.download('punkt')
+
 
 class Controller:
         directory = ""
         manager = DBManager
         parser = HTMLParser
+
         def __init__(self):
                 print("Controller init")
                 self.directory = "docrepository"
@@ -33,24 +34,25 @@ class Controller:
         def setup(self):
                 print("Found the following files:")
                 p = Path(self.directory)
-                for i in p.iterdir(): #glob('*.*'):
+                for i in p.iterdir():  # glob('*.*'):
                         print("Working on file:", i.name)
-                        path = Path.cwd().joinpath(self.directory+"\\"+i.name)
-                        with path.open('r', encoding='utf-8', errors='replace') as file:
+                        path = Path.cwd().joinpath(self.directory +
+                                                   "\\" + i.name)
+                        with path.open('r', encoding='utf-8',
+                                       errors='replace') as file:
                                 # Parser
                                 filetext = self.parser.parse(self, file)
-                                #Normalizer
-                                #Tokenizer
-                                #Save to DB
+                                # Normalizer
+                                # Tokenizer
+                                # Save to DB
                                 print(filetext)
                                 fdist = nltk.FreqDist(word_tokenize(filetext))
                                 print(fdist)
-                                #for term in fdist:
-                                        #self.manager.save(self, term)
-                queryfile = open('queryfile.txt','r')
+                                # for term in fdist:
+                                # self.manager.save(self, term)
+                queryfile = open('queryfile.txt', 'r')
                 queryArray = queryfile.read().splitlines()
                 print(queryArray)
-
 
         def displayResults(self):
                 print("display Results Method!")
