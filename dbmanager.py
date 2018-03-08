1800f8f97cbe16f494eb1ed845cdfd88ade0a2b0
Jorge Hevia <jmhev@outlook.com>
2018-03-08 15:27:50 +0100

    modified:   dbmanager.py - modified __init__ so it creates a DB with the needed collections - created saveDoc as intended - tested saveDoc
    TO-DO:
    - saveDoc error reporting and error correction

--- dbmanager.py
+++ dbmanager.py
@@ -1,6 +1,44 @@
+from pymongo import MongoClient
+
+
 class DBManager:
+    client = 1
+    db = 1
+    docs = 1
+    terms = 1
+    relations = 1
+
     def __init__(self):
         print("DBManager: Set up Database")
+        self.client = MongoClient('172.17.0.2:27017')
+        self.db = self.client.db
+        self.docs = self.db.docs
+        self.terms = self.db.terms
+        self.relations = self.db.relations
+
+        '''
+            docs {
+                'doc': {
+                    'name': 'doc'
+                }
+            }
+            ____
+            terms {
+                'term': {
+                    'term':'term',
+                    'ni': 10,
+                }
+            ____
+            relations {
+                'relation': {
+                    'doc': 'doc',
+                    'term': 'term',
+                    'tf': 3
+                }
+
+            }
+            }
+        '''
 
     '''
         Name: saveTerm
@@ -22,7 +60,7 @@ class DBManager:
     '''
 
     def saveDoc(self, doc):
-        print("DBManager: asked me to save:", doc)
+        self.docs.update({"name": doc}, {"name": doc}, upsert=True)
 
     '''
         Name: saveRelation
