faedee462773b25ab9d52e7fa0cf4f3e10551435
Jorge Hevia <jmhev@outlook.com>
2018-03-15 12:18:09 +0100

    modified:   search.py - Added function to make all the calcs, still needs testing. - TO-DO: add all the query values needed for TF.IDF 	modified:   testCalcTF.py - Implemented a way to show the lists sorted by document

--- search.py
+++ search.py
@@ -1,5 +1,6 @@
 #!venv/bin/python
 from math import sqrt
+from pymongo import DESCENDING
 '''
     Name:
     Input:
@@ -14,7 +15,7 @@ from math import sqrt
             the collection relations from the DB
            docs, which is a DBManager object which can access
             the collection documents from the DB
-    Output: a list of dict {'doc': docname, 'scalarTF': value}
+    Output: a list of dict {'doc': docname, 'cosTF': value, 'scalarTF': value}
     Function: the function run the following calculation
              (sim(dj,q) = dj * q = Σ Wij * Wiq),
              getting the TF values for earch term in the given
@@ -28,7 +29,8 @@ def calcTF(query, relations, docs):
     for doc in docs.find():
         tf_sum = 0
         tf_cos_sum = 0
-        terms = list(relations.find({'doc': doc['name']}))
+        terms = list(relations.find(
+            {'doc': doc['name']}))
         for term in terms:
             if term['term'] in query:
                 tf_sum = tf_sum + int(term['tf'])
@@ -42,4 +44,52 @@ def calcTF(query, relations, docs):
             cosTF = 0
         tf_total.append({'doc': doc['name'], 'cosTF': cosTF,
                          'scalarTF': tf_sum})
+    return tf_total
+
+
+'''
+    Name: calcAll
+    Input: query, which is an array of the words from the query
+           relation, which is a DBManager object which can access
+            the collection relations from the DB
+           docs, which is a DBManager object which can access
+            the collection documents from the DB,
+           terms, which is a DBManager object which can access
+           the collection terms from the DB
+    Output: a list of dict {'doc': docname,'cosTF': value,'scalarTF': value,
+                            'cosTF_IDF': value, 'scalarTF_IDF': value}
+    Function: the function runs all the calculations MUAJAJAJAJAJA
+'''
+
+
+def calcAll(query, relations, docs, terms):
+    tf_total = []
+    query_cos_div = sqrt(len(query))
+    for doc in docs.find():
+        scalarTF = 0
+        scalarTF_IDF = 0
+        tf_cos_sum = 0
+        tf_idf_cos_sum = 0
+        query_cos_div = 0
+        terms = list(relations.find({'doc': doc['name']}))
+        for term in terms:
+            if term['term'] in query:
+                termIDF = terms.find_one({'term': term['term']})['idf']
+                scalarTF = scalarTF + int(term['tf'])
+                tf_cos_sum = tf_cos_sum + int(term['tf']) ** 2
+                weightIDF = int(term['tf']) * termIDF
+                tf_idf_cos_sum = tf_idf_cos_sum + weightIDF ** 2
+                scalarTF_IDF = scalarTF_IDF + weightIDF * termIDF
+                print("match: " + term['term'])
+        try:
+            tf_cos_div = sqrt(tf_cos_sum)
+            tf_idf_cos_div = sqrt(tf_idf_cos_sum)
+            cosTF = scalarTF / (tf_cos_div * query_cos_div)
+            cosTF_IDF = scalarTF_IDF / (tf_idf_cos_div / query_cos_div)
+        except ZeroDivisionError:
+            tf_cos_div = 0
+            cosTF = 0
+        tf_total.append({'doc': doc['name'], 'cosTF': cosTF,
+                         'scalarTF': scalarTF, 'cosTF_IDF': cosTF_IDF,
+                         'scalarTF_IDF': scalarTF_IDF})
     return tf_total
--- testCalcTF.py
+++ testCalcTF.py
@@ -1,11 +1,13 @@
 #!venv/bin/python
 from dbmanager import DBManager
+from operator import itemgetter
 import search
 
 m = DBManager()
 
 iquery = ['want', 'second', 'victory', 'guest', 'rica']
-result = search.calcTF(iquery, m.relations, m.docs)
+result = sorted(search.calcTF(iquery, m.relations, m.docs),
+                key=itemgetter('doc'))
 
 for r in result:
     print(r)
